4b4baf5b513bd1fbc1054c4263bc4753899125c1
ayushambar <45435726+ayushambar@users.noreply.github.com>
2020-11-05 20:55:42 +0530

    Add files via upload

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -11,8 +11,36 @@ class Signup extends Component {
 		}
 	}
 
+	handleChange = name => event => {
+		this.setState({ [name]: event.target.value});
+	};
+
+	clickSubmit = event => {
+		event.preventDefault();
+		const {name,email,password} = this.state;
+		const user = {
+			name,
+			email,
+			password
+		};
+		//console.log(user);
+		fetch("http://localhost:3000/signup", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(user)
+		})
+		.then(response => {
+			return response.json()
+		})
+		.catch(err => console.log(err))
+	};
 
 	render() {
+		const {name,email,password} = this.state;
+
 		return (
 			<div className='container'>
 			<h2 className='mt-5 mb-5'>Signup</h2>
@@ -20,17 +48,17 @@ class Signup extends Component {
 				<form>
 					<div className='form-group'>
 						<label className='text-muted'>Name</label>
-						<input type='text' className='form-control' />
+						<input onChange={this.handleChange('name')} type='text' className='form-control' value={name} />
 					</div>
 					<div className='form-group'>
 						<label className='text-muted'>Email</label>
-						<input type='email' className='form-control' />
+						<input onChange={this.handleChange('email')} type='email' className='form-control' value={email} />
 					</div>
 					<div className='form-group'>
 						<label className='text-muted'>Password</label>
-						<input type='password' className='form-control' />
+						<input onChange={this.handleChange('password')} type='password' className='form-control' value={password} />
 					</div>
-					<button className='btn btn-raised btn-primary'>Submit</button>
+					<button onClick={this.clickSubmit} className='btn btn-raised btn-primary'>Submit</button>
 				</form>
 			</div>
 		);
