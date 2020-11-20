62ad0fb501212c09f191ce0442cce63b7e177c5b
Ayush Ambar <ayushambar4@gmail.com>
2020-11-20 23:29:51 +0530

    Profile photo upload feature - frontend. Yet to handle FormData at the backend.

--- src/user/EditProfile.js
+++ src/user/EditProfile.js
@@ -30,6 +30,7 @@ class EditProfile extends Component {
   }
 
   componentDidMount() {
+    this.userData = new FormData();
     const userId = this.props.match.params.userId;
     this.init(userId);
   }
@@ -52,7 +53,9 @@ class EditProfile extends Component {
   }
 
   handleChange = name => event => {
-    this.setState({ [name]: event.target.value});			//revise syntax
+    const value = name === 'photo' ? event.target.files[0] : event.target.value;
+    this.userData.set(name,value);
+    this.setState({ [name]: value});			//revise syntax
   };
 
   clickSubmit = event => {
@@ -68,7 +71,7 @@ class EditProfile extends Component {
       const userId = this.props.match.params.userId;
       const token = isAuthenticated().token;
 
-       update(userId,token,user).then(data => {
+       update(userId,token,this.userData).then(data => {
          if(data?.error) this.setState({ error: data.error });
          else this.setState({
             redirectToProfile: true
@@ -79,6 +82,10 @@ class EditProfile extends Component {
 
   signupForm = (name,email,password) => (
     <form>
+    <div className='form-group'>
+      <label className='text-muted'>Profile Photo</label>
+      <input onChange={this.handleChange('photo')} type='file' accept='image/*' className='form-control' />
+    </div>
       <div className='form-group'>
         <label className='text-muted'>Name</label>
         <input onChange={this.handleChange('name')} type='text' className='form-control' value={name} />
