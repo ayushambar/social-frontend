1fb3e3718decf0d095217441580718b1f3a84187
Ayush Ambar <ayushambar4@gmail.com>
2020-12-04 02:04:24 +0530

    Follow feature added.

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -4,14 +4,40 @@ import {Redirect, Link} from 'react-router-dom';
 import {read} from './apiUser';
 import DefaultProfile from '../images/avatar.png';
 import DeleteUser from './DeleteUser';
+import FollowProfileButton from './FollowProfileComponent';
 
 class Profile extends Component {
   constructor(){
     super()
     this.state = {
-      user: "",
-      redirectToSignin: false
+      user: {following: [], followers: []},
+      redirectToSignin: false,
+      following: false,
+      error: ""
     }
+  }
+
+  checkFollow = user => {
+    const jwt = isAuthenticated();
+    const match = user.followers.find(follower => {
+      //one id has many followers and vice versa
+      return follower._id === jwt.user._id
+    })
+    return match;
+  }
+
+  clickFollowButton = callApi => {
+    const userId = isAuthenticated().user._id;
+    const token = isAuthenticated().token;
+
+    callApi(userId, token, this.state.user._id)
+    .then(data => {
+      if(data.error){
+        this.setState({error: data.error})
+      } else{
+        this.setState({user: data, following: !this.state.following})
+      }
+    })
   }
 
   init = (userId) => {
@@ -21,7 +47,8 @@ class Profile extends Component {
       if(data.error){
         this.setState({redirectToSignin:true});
       } else {
-        this.setState({user: data});
+        let following = this.checkFollow(data);
+        this.setState({user: data, following});
       }
     })
   }
@@ -64,14 +91,17 @@ class Profile extends Component {
             <p>{`Joined ${new Date(user.created).toDateString()}`}</p>
           </div>
 
-            {isAuthenticated().user && isAuthenticated().user._id === user._id && (
+            {isAuthenticated().user && isAuthenticated().user._id === user._id ? (
               <div className="d-inline-block">
                 <Link className="btn btn-raised btn-success mr-5" to={`/user/edit/${user._id}`}>
                   Edit Profile
                 </Link>
                 <DeleteUser userId={user._id}/>
               </div>
-            )}
+            ) : ( <FollowProfileButton
+              following={this.state.following}
+              onButtonClick={this.clickFollowButton}
+               />)}
           </div>
         </div>
         <div className="row">
--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -55,3 +55,19 @@ export const list = () => {
   })
   .catch(err => console.log(err));
 }
+
+export const follow =(userId,token, followId) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/user/follow`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({userId, followId})
+  })
+  .then(response => {
+    return response.json();
+  })
+  .catch(err => console.log(err));
+};
