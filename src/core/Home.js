ad5ba7225c3b35bb9656302b1c219f5c4044eb6d
Ayush Ambar <ayushambar4@gmail.com>
2020-12-08 03:03:35 +0530

    ..

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import Posts from "../post/Posts";
 
 const Home = () => (
-	<div className='jumbotron'>
-		<h2>Home</h2>
-		<p className='lead'>Welcome to react frontend</p>
-	</div>
-)
+  <div>
+    <div className="jumbotron">
+      <h2>Home</h2>
+      <p className="lead">Welcome to React Frontend</p>
+    </div>
+    <div className="container">
+      <Posts />
+    </div>
+  </div>
+);
 
 export default Home;
