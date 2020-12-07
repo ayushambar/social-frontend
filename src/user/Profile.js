5b933b4efdee40ee7d18e7de824f00c7910bd760
Ayush Ambar <ayushambar4@gmail.com>
2020-12-07 23:11:28 +0530

    Progress in profile page, and for creating post

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,58 +1,71 @@
-import React, {Component} from 'react';
-import {isAuthenticated} from '../auth';
-import {Redirect, Link} from 'react-router-dom';
-import {read} from './apiUser';
-import DefaultProfile from '../images/avatar.png';
-import DeleteUser from './DeleteUser';
-import FollowProfileButton from './FollowProfileComponent';
-import ProfileTabs from './ProfileTabs';
+import React, { Component } from "react";
+import { isAuthenticated } from "../auth";
+import { Redirect, Link } from "react-router-dom";
+import { read } from "./apiUser";
+import DefaultProfile from "../images/avatar.png";
+import DeleteUser from "./DeleteUser";
+import FollowProfileButton from "./FollowProfileComponent";
+import ProfileTabs from "./ProfileTabs";
+import { listByUser } from "../post/apiPost";
 
 class Profile extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
-      user: {following: [], followers: []},
+      user: { following: [], followers: [] },
       redirectToSignin: false,
       following: false,
-      error: ""
-    }
+      error: "",
+      posts: []
+    };
   }
 
+  // check follow
   checkFollow = user => {
     const jwt = isAuthenticated();
     const match = user.followers.find(follower => {
-      //one id has many followers and vice versa
-      return follower._id === jwt.user._id
-    })
+      // one id has many other ids (followers) and vice versa
+      return follower._id === jwt.user._id;
+    });
     return match;
-  }
+  };
 
   clickFollowButton = callApi => {
     const userId = isAuthenticated().user._id;
     const token = isAuthenticated().token;
 
-    callApi(userId, token, this.state.user._id)
-    .then(data => {
-      if(data.error){
-        this.setState({error: data.error})
-      } else{
-        this.setState({user: data, following: !this.state.following})
+    callApi(userId, token, this.state.user._id).then(data => {
+      if (data.error) {
+        this.setState({ error: data.error });
+      } else {
+        this.setState({ user: data, following: !this.state.following });
       }
-    })
-  }
+    });
+  };
 
-  init = (userId) => {
+  init = userId => {
     const token = isAuthenticated().token;
-    read(userId,token)
-    .then(data => {
-      if(data.error){
-        this.setState({redirectToSignin:true});
+    read(userId, token).then(data => {
+      if (data.error) {
+        this.setState({ redirectToSignin: true });
       } else {
         let following = this.checkFollow(data);
-        this.setState({user: data, following});
+        this.setState({ user: data, following });
+        this.loadPosts(data._id);
       }
-    })
-  }
+    });
+  };
+
+  loadPosts = userId => {
+    const token = isAuthenticated().token;
+    listByUser(userId, token).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        this.setState({ posts: data });
+      }
+    });
+  };
 
   componentDidMount() {
     const userId = this.props.match.params.userId;
@@ -64,58 +77,100 @@ class Profile extends Component {
     this.init(userId);
   }
 
-  render(){
-    const {redirectToSignin,user} = this.state;
-    if(redirectToSignin) return <Redirect to="/signin" />
+  render() {
+    const { redirectToSignin, user, posts } = this.state;
+    if (redirectToSignin) return <Redirect to="/signin" />;
 
-    const photoUrl = user._id ? `${process.env.REACT_APP_API_URL}/user/photo/${user._id}?${new Date().getTime()}` : DefaultProfile
+    const photoUrl = user._id
+      ? `${process.env.REACT_APP_API_URL}/user/photo/${
+          user._id
+        }?${new Date().getTime()}`
+      : DefaultProfile;
 
-    return(
+    return (
       <div className="container">
-        <h2 className='mt-5 mb-5'>Profile</h2>
+        <h2 className="mt-5 mb-5">Profile</h2>
         <div className="row">
-          <div className="div-md-6">
-
-            <img style={{height : "200px", width: "auto"}}
-            className="img-thumbnail mr-5"
-            src={photoUrl}
-            onError={i => (i.target.src = `${DefaultProfile}`)}
-            alt={user.name} />
-
+          <div className="col-md-4">
+            <img
+              style={{ height: "200px", width: "auto" }}
+              className="img-thumbnail"
+              src={photoUrl}
+              onError={i => (i.target.src = `${DefaultProfile}`)}
+              alt={user.name}
+            />
           </div>
 
-          <div className="div-md-6">
+          <div className="col-md-8">
+            <div className="lead mt-2">
+              <p>Hello {user.name}</p>
+              <p>Email: {user.email}</p>
+              <p>{`Joined ${new Date(user.created).toDateString()}`}</p>
+            </div>
 
-          <div className="lead mt-2">
-            <p>{user.name}</p>
-            <p>Email: {user.email}</p>
-            <p>{`Joined ${new Date(user.created).toDateString()}`}</p>
-          </div>
-
-            {isAuthenticated().user && isAuthenticated().user._id === user._id ? (
+            {isAuthenticated().user &&
+            isAuthenticated().user._id === user._id ? (
               <div className="d-inline-block">
-                <Link className="btn btn-raised btn-success mr-5" to={`/user/edit/${user._id}`}>
+                <Link
+                  className="btn btn-raised btn-info mr-5"
+                  to={`/post/create`}
+                >
+                  Create Post
+                </Link>
+
+                <Link
+                  className="btn btn-raised btn-success mr-5"
+                  to={`/user/edit/${user._id}`}
+                >
                   Edit Profile
                 </Link>
-                <DeleteUser userId={user._id}/>
+                <DeleteUser userId={user._id} />
               </div>
-            ) : ( <FollowProfileButton
-              following={this.state.following}
-              onButtonClick={this.clickFollowButton}
-               />)}
-               <hr />
-               <ProfileTabs followers={user.followers} following={user.following}/>
+            ) : (
+              <FollowProfileButton
+                following={this.state.following}
+                onButtonClick={this.clickFollowButton}
+              />
+            )}
+
+            <div>
+              {isAuthenticated().user &&
+                isAuthenticated().user.role === "admin" && (
+                  <div class="card mt-5">
+                    <div className="card-body">
+                      <h5 className="card-title">Admin</h5>
+                      <p className="mb-2 text-danger">
+                        Edit/Delete as an Admin
+                      </p>
+                      <Link
+                        className="btn btn-raised btn-success mr-5"
+                        to={`/user/edit/${user._id}`}
+                      >
+                        Edit Profile
+                      </Link>
+                      {/*<DeleteUser userId={user._id} />*/}
+                      <DeleteUser />
+                    </div>
+                  </div>
+                )}
+            </div>
           </div>
         </div>
         <div className="row">
           <div className="col md-12 mt-5 mb-5">
-            <hr/>
+            <hr />
             <p className="lead">{user.about}</p>
-            <hr/>
+            <hr />
+
+            <ProfileTabs
+              followers={user.followers}
+              following={user.following}
+              posts={posts}
+            />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
